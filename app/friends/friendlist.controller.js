19f78b1ef4d4031c2e8bf4a6c6d55cff40def9e8
Margaret Clinard <margaret.clinard@gmail.com>
2015-03-23 16:22:08 -0500

    refactored code to loop through friend list object to get all friend data

--- app/friends/friendlist.controller.js
+++ app/friends/friendlist.controller.js
@@ -6,12 +6,38 @@ function FriendController ($http) {
   var vm = this;
   var fb = new Firebase('https://presently-surprised.firebaseio.com/');
 
-  vm.users = {};
+  vm.friends = {};
 
   $http
     .get('https://presently-surprised.firebaseio.com/users/' + fb.getAuth().uid + '/friends.json')
     .success(function (data){
-      vm.users = data;
-      console.log(data)
+      vm.friends = data;
+    })
+    .then(function (res) {
+      console.log(res.data);
+      Object.keys(res.data).forEach(function (key) {
+        friendPop(key, res.data[key].uid)
+      });
     });
+
+  vm.getWishList = function (uid) {
+    console.log('uid', uid);
+
+    $http
+      .get('https://presently-surprised.firebaseio.com/users/' + uid + '/wishlist.json')
+      .success(function (data) {
+        console.log(data);
+      })
+  };
+
+  function friendPop(uuid, uid) {
+    $http
+      .get('https://presently-surprised.firebaseio.com/users/' + uid + '.json')
+      .success(function (data){
+        console.log('friends', data);
+        data.uid = uid;
+        vm.friends[uuid] = data;
+      });
+  }
+
 }
